4cf613bc4a3fc20600473501ab8309da16317c96
StarrySkyWorld <2061961813@qq.com>
2019-05-16 21:02:24 +0800

    Update Study ImageView

--- app/src/main/java/com/example/studyapp_1/ImageViewActivity.java
+++ app/src/main/java/com/example/studyapp_1/ImageViewActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.bumptech.glide.Glide;
+
 public class ImageViewActivity extends AppCompatActivity {
 
     private ImageView imageView;
@@ -13,6 +15,6 @@ public class ImageViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_image_view);
         imageView=findViewById(R.id.imageView_4);
-
+        Glide.with(this).load("https://abc.2008php.com/2017_Website_appreciate/2017-09-16/20170916221628.jpg").into(imageView);
     }
 }
